d1a5e42d7d962b1b3d17ace0f060474633305e0d
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2024-06-02 15:29:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const output = document.getElementById("output");
+const submitBtn = document.getElementById("btn");
+
+const handleSubmit = () => {
+	const textValue = text.value;
+	const delayValue = delay.value;
+
+	setTimeout(() => {
+		output.innerHTML = textValue;
+	}, delayValue)
+}
+
+submitBtn.addEventListener("click", handleSubmit)
